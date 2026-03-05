98cd5c7baece1878174b478ab8e8fb18c18c468b
Vasilii Rogin <vasya@rogin.ru>
2026-03-05 18:30:08 +0200

    Add fakechain into network type

--- monero-wasm-module/walletApi.ts
+++ monero-wasm-module/walletApi.ts
@@ -162,6 +162,7 @@ export const NetworkType = {
   MAINNET: 0,
   TESTNET: 1,
   STAGENET: 2,
+  FAKECHAIN: 3,
 } as const;
 
 export type NetworkType = (typeof NetworkType)[keyof typeof NetworkType];
